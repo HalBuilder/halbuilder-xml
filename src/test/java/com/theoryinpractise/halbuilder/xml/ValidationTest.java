205e052bce0bc477083462a3f1555b8dcc116b4b
Mark Derricutt <mark@talios.com>
2013-05-27 22:43:54 +1200

    Added tests from core

---
+++ src/test/java/com/theoryinpractise/halbuilder/xml/ValidationTest.java
@@ -0,0 +1,41 @@
+package com.theoryinpractise.halbuilder.xml;
+
+import com.theoryinpractise.halbuilder.api.Contract;
+import com.theoryinpractise.halbuilder.api.ReadableRepresentation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import org.testng.annotations.Test;
+
+import java.io.InputStreamReader;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class ValidationTest {
+
+
+    RepresentationFactory representationFactory = new XmlRepresentationFactory();
+
+    ReadableRepresentation representation = representationFactory.readRepresentation(
+            new InputStreamReader(ValidationTest.class.getResourceAsStream("/example.xml")));
+
+    @Test
+    public void testValidation() {
+
+        Contract noWhiteSpaceInName = new Contract() {
+            public boolean isSatisfiedBy(ReadableRepresentation resource) {
+                return (((String) resource.getValue("name", "")).matches("\\W*"));
+            }
+        };
+
+        Contract anyCharsInName = new Contract() {
+            public boolean isSatisfiedBy(ReadableRepresentation resource) {
+                return (((String) resource.getValue("name", "")).matches(".*"));
+            }
+        };
+
+        assertThat(representation.isSatisfiedBy(noWhiteSpaceInName)).isFalse();
+        assertThat(representation.isSatisfiedBy(anyCharsInName)).isTrue();
+
+    }
+
+
+}
